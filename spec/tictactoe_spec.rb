1be031c0159583810828b55601d0d84773ffe041
agathasta <kalidonia@hotmail.com>
2020-10-29 12:30:31 +0100

    Add class Player

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,5 +1,25 @@
 # spec/tictactoe_spec.rb
+
+# to run: bundle exec rspec
+
 require "tictactoe"
 
 describe TicTacToe do
+	describe Player do
+    context "#initialize" do
+			it "gives error when empty" do
+				expect { Player.new({}) }.to raise_error
+      end
+    end
+	end
 end
+
+# describe Player do
+# 	context "#initialize" do
+
+# 		it "raises an exception when initialized with {}" do
+# 			expect { Player.new({}) }.to raise_error
+# 		end
+
+# 	end
+# end
