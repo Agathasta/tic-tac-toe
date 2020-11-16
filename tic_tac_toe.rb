7648638e2a8665976287c1d85dafc2c669e4c80e
agathasta <kalidonia@hotmail.com>
2020-11-17 00:44:32 +0100

    Add small rubocop adjustements

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,19 +1,22 @@
-class Game # controls the flow of the game
+# frozen_string_literal: true
+
+# controls the flow of the game
+class Game
   def initialize
     @board = Board.new
-    @player_1 = Player.new("Player 1", "X", @board)
-    @player_2 = Player.new("Player 2", "O", @board)
-    @current_player = @player_1
+    @player1 = Player.new('Player 1', 'X', @board)
+    @player2 = Player.new('Player 2', 'O', @board)
+    @current_player = @player1
     welcome
   end
 
   def welcome
     puts
-    puts "Welcome to exciting Tic Tac Toe"
-    puts "(I am going to assume you know the rules)"
-    puts "This is the board:"
+    puts 'Welcome to exciting Tic Tac Toe'
+    puts '(I am going to assume you know the rules)'
+    puts 'This is the board:'
     @board.display
-    puts "Enjoy!"
+    puts 'Enjoy!'
   end
 
   def play
@@ -21,6 +24,7 @@ class Game # controls the flow of the game
       @current_player.get_move
       @board.display
       break if game_over?
+
       switch_player
     end
   end
@@ -40,7 +44,7 @@ class Game # controls the flow of the game
 
   def check_tie
     if @board.full_board?
-      puts "Ohhhhhh... a tie"
+      puts 'Ohhhhhh... a tie'
       true
     else
       false
@@ -48,10 +52,11 @@ class Game # controls the flow of the game
   end
 
   def switch_player
-    @current_player == @player_1 ? @current_player = @player_2 : @current_player = @player_1
+    @current_player = @current_player == @player1 ? @player2 : @player1
   end
 end
 
+# gets input from players
 class Player
   attr_accessor :name, :token
 
@@ -64,30 +69,28 @@ class Player
   def get_move
     loop do
       move = ask_move
-      if correct_format?(move)
-        if @board.add_token(move, @token)
-          break
-        end
-      end
+      next unless correct_format?(move)
+      break if @board.add_token(move, @token)
     end
   end
 
   def ask_move
     puts "#{@name}, choose your move (a number 1 - 9):"
-    print "> "
-    (gets.chomp.to_i) - 1
+    print '> '
+    gets.chomp.to_i - 1
   end
 
   def correct_format?(move)
     if (0..8).include?(move)
       true
     else
-      puts "Wrong input!"
+      puts 'Wrong input!'
       false
     end
   end
 end
 
+# displays and saves the board, checks game status
 class Board
   def initialize
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -109,27 +112,27 @@ class Board
     if valid_move?(move)
       @board[move] = token
     else
-      puts "That move is already taken"
+      puts 'That move is already taken'
     end
   end
 
   def valid_move?(move)
-    @board[move].class == Integer
+    @board[move].instance_of?(Integer)
   end
 
   def full_board?
-    @board.all? { |c| c.class == String}
+    @board.all? { |c| c.instance_of?(String) }
   end
 
   def winning_combos
     [[@board[0], @board[1], @board[2]],
-    [@board[3], @board[4], @board[5]],
-    [@board[6], @board[7], @board[8]],
-    [@board[0], @board[3], @board[6]],
-    [@board[1], @board[4], @board[7]],
-    [@board[2], @board[5], @board[8]],
-    [@board[0], @board[4], @board[8]],
-    [@board[2], @board[4], @board[6]]]
+     [@board[3], @board[4], @board[5]],
+     [@board[6], @board[7], @board[8]],
+     [@board[0], @board[3], @board[6]],
+     [@board[1], @board[4], @board[7]],
+     [@board[2], @board[5], @board[8]],
+     [@board[0], @board[4], @board[8]],
+     [@board[2], @board[4], @board[6]]]
   end
 
   def winning_combination?(token)
